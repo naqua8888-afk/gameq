5135bd77f7acfcf81afacd7ce3f22d75deca041f
anastasiia.pryimak <anastasiia.pryimak@mirko.in.ua>
2022-03-14 17:16:03 +0200

    Menu click outside

--- js/script.js
+++ js/script.js
@@ -3,12 +3,22 @@ window.onload = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.querySelector(".nav-header").classList.add("scrolled")
+        document.querySelector(".nav-header").classList.add("scrolled");
+        MenuElement.classList.remove('fold')
     } else {
         document.querySelector(".nav-header").classList.remove("scrolled");
     }
 }
 
+var MenuElement = document.querySelector('.nav-header');
+
 function toggleMenu() {
-    document.querySelector('.nav-header').classList.toggle('fold')
+    MenuElement.classList.toggle('fold')
 }
+
+document.addEventListener('click', function(event) {
+    var isClickInsideElement = MenuElement.contains(event.target);
+    if (!isClickInsideElement) {
+        MenuElement.classList.remove('fold')
+    }
+});
